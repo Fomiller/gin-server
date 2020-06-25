1008a78b9a8d1976e144ec02245dc1c269fa5049
Forrest <Forrestmillerj@gmail.com>
2020-06-24 20:48:10 -0500

    added endpoint groups and basic authentication to select enpoints

--- main.go
+++ main.go
@@ -6,13 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var secrets = gin.H{
+	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
+	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
+	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
+}
+
 func main() {
 	router := gin.Default()
-	router.LoadHTMLFiles("index.gohtml")
+	// router.LoadHTMLFiles("templates/index.gohtml")
+	router.LoadHTMLGlob("templates/*")
 	router.GET("/", indexHandler)
 	router.GET("/welcome", welcomeHandler)
 	router.GET("/welcome2", welcomeHandler2)
 	router.GET("/file", fileHandler)
+
+	// secret := router.Group("/secret")
+	// {
+	// 	secret.GET("/clubhouse", secretHandler)
+	// 	secret.GET("/clubhouse/:password", clubHouseHandler)
+	// }
+	authorized := router.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo":    "bar",
+		"austin": "1234",
+		"lena":   "hello2",
+		"manu":   "4321",
+	}))
+	authorized.GET("/secrets", secretsHandler)
 
 	router.Run()
 }
@@ -42,5 +62,35 @@ func welcomeHandler2(c *gin.Context) {
 }
 
 func fileHandler(c *gin.Context) {
-	f
+	data := gin.H{
+		"firstName": "Forrest",
+		"lastName":  "Miller",
+	}
+
+	c.HTML(http.StatusOK, "index.gohtml", data)
+}
+
+// func secretHandler(c *gin.Context) {
+// 	c.HTML(http.StatusOK, "secret.gohtml", nil)
+// }
+
+// func clubHouseHandler(c *gin.Context) {
+// 	password := c.Query("password")
+// 	c.HTML(http.StatusOK, "clubHouse.gohtml", password)
+// }
+
+func secretsHandler(c *gin.Context) {
+	user := c.MustGet(gin.AuthUserKey).(string)
+	if secret, ok := secrets[user]; ok {
+		c.HTML(http.StatusOK, "clubhouse.gohtml", gin.H{
+			"user":   user,
+			"secret": secret,
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"user":   user,
+			"secret": "NO SECRET :(",
+		})
+	}
+
 }
